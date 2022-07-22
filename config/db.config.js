dc3869cf92817335ace36b4e00a4bf100cfbcfa6
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-07-22 17:48:00 +0200

    feat: views responsive and favorite button added

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 mongoose
-    .connect("mongodb+srv://Ivan:VM6ivYYS6r6ekGD@cluster0.ybodnrm.mongodb.net/test")
+    .connect("mongodb://localhost/tv-tracker")
     .then(() => console.info("Connected do DB"))
     .catch(() => console.error("Error DB", error))
+
+
+// DB Iván: "mongodb+srv://Ivan:VM6ivYYS6r6ekGD@cluster0.ybodnrm.mongodb.net/test"
+// DB Adrián: "mongodb://localhost/tv-tracker"
