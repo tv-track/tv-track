1beb73acb5ba2a195081635fc8f580b02c114b13
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-07-26 13:28:37 +0200

    feat: session config done

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const createError = require("http-errors")
+const createError = require("http-errors");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 app.use(express.static(`${__dirname}/public`));
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-require("./config/db.config")
-require("./config/hbs.config")
+require("./config/db.config");
+require("./config/hbs.config");
+
+const { session, loadUser } = require("./config/session.config");
+app.use(session);
+app.use(loadUser);
 
 const routes = require("./config/routes.config");
 app.use("/", routes);
 
 app.use((req, res, next) => {
-    next(createError(404, "Page not found"))
+  next(createError(404, "Page not found"));
 });
 
 app.use((error, req, res, next) => {
-    console.error(error)
-    const message = error.message
-    const metadata = app.get("env") === "development" ? error : {}
-    const status = error.status || 500
-    res.status(status)
-        .render(`errors/500`, { message, metadata })
-})
-
+  console.error(error);
+  const message = error.message;
+  const metadata = app.get("env") === "development" ? error : {};
+  const status = error.status || 500;
+  res.status(status).render(`errors/500`, { message, metadata });
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Application listening at port ${port}`));
---
+++ config/session.config.js
@@ -0,0 +1,33 @@
+const expressSession = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
+const { User } = require("../models");
+
+const session = expressSession({
+  secret: process.env.SESSION_SECRET || "super secret",
+  resave: false,
+  saveUninitialized: false,
+  name: "session cookie",
+  store: MongoStore.create({
+    mongoUrl: mongoose.connection._connectionString,
+    ttl: 24 * 3600,
+  }),
+  cookie: {
+    secure: process.env.SESSION_SECURE === "true",
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  },
+});
+
+const loadUser = (req, res, next) => {
+  const { userId } = req.session;
+  User.findById(userId)
+    .then((user) => {
+      req.user = user;
+      res.locals.currentUser = user;
+      next();
+    })
+    .catch((error) => next(error));
+};
+
+module.exports = { session, loadUser };
