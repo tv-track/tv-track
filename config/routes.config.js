d113eeb3d54f58aee597bee511d3c4306bf6029c
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-07-26 20:12:32 +0200

    feat: routes secured

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { series, auth } = require("../controllers");
+const secure = require("../middlewares/secure.mid")
+const isUser = secure.isAuthenticated
+const isAdmin = secure.isAdmin
 
 router.get("/", series.list);
-router.get("/serie/:id", series.detail);
+router.get("/serie/:id", isUser, series.detail);
 
-router.get("/new-serie", series.newSerie);
-router.post("/new-serie", series.createSerie);
-router.get("/serie/:serieId/new-episode", series.createEpisode);
-router.post("/serie/:serieId/new-episode", series.doCreateEpisode);
+router.get("/new-serie", isAdmin, series.newSerie);
+router.post("/new-serie", isAdmin, series.createSerie);
+router.get("/serie/:serieId/new-episode", isAdmin, series.createEpisode);
+router.post("/serie/:serieId/new-episode", isAdmin, series.doCreateEpisode);
 
 router.get("/register", auth.register);
 router.post("/register", auth.doRegister);
---
+++ middlewares/secure.mid.js
@@ -0,0 +1,15 @@
+module.exports.isAuthenticated = (req, res, next) => {
+    if (req.user) {
+        next()
+    } else {
+        res.redirect("/login")
+    }
+}
+
+module.exports.isAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        next()
+    } else {
+        res.redirect("/")
+    }
+}
