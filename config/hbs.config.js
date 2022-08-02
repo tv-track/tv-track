c9f8b9e959e586b446a37d1e39793c48287e1a8c
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-08-02 22:09:17 +0200

    feat: hbs helper almost added

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,3 +1,13 @@
-const hbs = require ("hbs")
+const hbs = require("hbs");
+hbs.handlebars === require('handlebars');
+const { User } = require("../models");
 
-hbs.registerPartials(__dirname + "/../views/partials")
+hbs.registerPartials(__dirname + "/../views/partials");
+
+hbs.registerHelper("following", function (serie, options) {
+  if (serie) {
+    options.fn(this);
+  } else {
+    options.inverse(this);
+  }
+});
