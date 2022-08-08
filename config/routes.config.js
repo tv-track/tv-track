f3b751dee29aaeea6169c670082615defda84b24
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-08-08 19:17:47 +0200

    fix: new column width on series edit view and about us view, feat: carousel on index view

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { series, auth, match, viewed, about } = require("../controllers");
-const secure = require("../middlewares/secure.mid")
-const isUser = secure.isAuthenticated
-const isAdmin = secure.isAdmin
+const secure = require("../middlewares/secure.mid");
+const isUser = secure.isAuthenticated;
+const isAdmin = secure.isAdmin;
 
 router.get("/", series.list);
 router.get("/series/:id", isUser, series.detail);
@@ -12,28 +12,28 @@ router.get("/new-serie", isAdmin, series.newSerie);
 router.post("/new-serie", isAdmin, series.createSerie);
 router.get("/series/:serieId/new-episode", isAdmin, series.createEpisode);
 router.post("/series/:serieId/new-episode", isAdmin, series.doCreateEpisode);
-router.post("/series/:serieId/delete", isAdmin, series.delete)
-router.get("/series/:serieId/edit", isAdmin, series.update)
-router.post("/series/:serieId/edit", isAdmin, series.doUpdate)
-router.post("/episodes/:episodeId/delete", isAdmin, series.doDeleteEpisode)
-router.post("/series/search", isUser, series.search)
-router.post("/series/search-genre", series.searchByGenre)
-
+router.post("/series/:serieId/delete", isAdmin, series.delete);
+router.get("/series/:serieId/edit", isAdmin, series.update);
+router.post("/series/:serieId/edit", isAdmin, series.doUpdate);
+router.post("/episodes/:episodeId/delete", isAdmin, series.doDeleteEpisode);
+router.post("/series/search", isUser, series.search);
+router.post("/series/search-genre", series.searchByGenre);
 
 router.get("/register", auth.register);
 router.post("/register", auth.doRegister);
 router.get("/login", auth.login);
 router.post("/login", auth.doLogin);
 
-router.get("/logout", auth.doLogout)
-router.get("/users/:id", isUser, auth.profile)
-router.get("/users/:id/edit-user", isUser, auth.editUser)
-router.post("/users/:id/edit-user", isUser, auth.doEditUser)
+router.get("/logout", auth.doLogout);
+router.get("/users/:id", isUser, auth.profile);
+router.get("/users/:id/edit-user", isUser, auth.editUser);
+router.post("/users/:id/edit-user", isUser, auth.doEditUser);
 
-router.post("/series/:serieId/follow", isUser, match.follow)
-router.post("/episodes/:episodeId/viewed", isUser, viewed.viewed)
+router.post("/series/:serieId/follow", isUser, match.follow);
+router.post("/episodes/:episodeId/viewed", isUser, viewed.viewed);
 
-router.get("/about/us", about.aboutUs)
-router.get("/about/ivan", about.aboutIvan)
+router.get("/about/us", about.aboutUs);
+router.get("/about/ivan", about.aboutIvan);
+router.get("/about/adrian", about.aboutAdrian);
 
 module.exports = router;
--- controllers/about.controller.js
+++ controllers/about.controller.js
@@ -1,9 +1,11 @@
-
-
 module.exports.aboutUs = (req, res, next) => {
-    res.render("about/us")
-}
+  res.render("about/us");
+};
 
 module.exports.aboutIvan = (req, res, next) => {
-    res.render("about/ivan")
-}
+  res.render("about/ivan");
+};
+
+module.exports.aboutAdrian = (req, res, next) => {
+  res.render("about/adrian");
+};
